5f1bfde2a8599c142c5ec75482588095446f99ae
Thomas Reidemeister <thomas@labforge.ca>
2023-06-07 17:17:05 -0400

    Copyright header in tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,22 @@
-//
-// Created by treideme on 01/06/23.
-//
+/******************************************************************************
+ *  Copyright 2023 Labforge Inc.                                              *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this project except in compliance with the License.        *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************
+
+@file test.cpp Integration Test suite for this driver
+@author Thomas Reidemeister <thomas@labforge.ca>
+*/
 #include <PvConfigurationReader.h>
 #include "rclcpp/rclcpp.hpp"
 #include "bottlenose_camera_driver.hpp"
