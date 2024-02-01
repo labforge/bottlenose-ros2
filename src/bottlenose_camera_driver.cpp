862371589d7ef213a781d3d7ea6628388bcc544b
Thomas Reidemeister <treideme@gmail.com>
2024-02-01 18:21:07 -0500

    optimized point decoding

--- src/bottlenose_camera_driver.cpp
+++ src/bottlenose_camera_driver.cpp
@@ -986,19 +986,19 @@ void CameraDriver::publish_bboxes(const bboxes_t &bboxes, const uint64_t &timest
     vision_msgs::msg::BoundingBox2D bbox;
     vision_msgs::msg::ObjectHypothesisWithPose hyp;
 
-#if ROS_VERSION_MAJOR == 2 && ROS_VERSION_MINOR == 0
-    bbox.center.x = bboxes.box[i].left + width / 2.0;
-    bbox.center.y = bboxes.box[i].top + height / 2.0;
     bbox.size_x = width;
     bbox.size_y = height;
 
+#if ROS_VERSION_MAJOR == 2 && ROS_VERSION_MINOR == 0
+    bbox.center.x = bboxes.box[i].left + width / 2.0;
+    bbox.center.y = bboxes.box[i].top + height / 2.0;
+
+    // Encode the class
     hyp.score = bboxes.box[i].score;
     hyp.id = bboxes.box[i].label;
 #else // Humble
     bbox.center.position.x = bboxes.box[i].left + width / 2.0;
     bbox.center.position.y = bboxes.box[i].top + height / 2.0;
-    bbox.size_x = width;
-    bbox.size_y = height;
 
     // Encode the class
     hyp.hypothesis.class_id = bboxes.box[i].label;
